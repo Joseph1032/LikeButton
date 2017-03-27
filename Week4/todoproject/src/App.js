dcd7cc42b0bf8962b115fce221c7749eb9ecc8c2
Josephine Gabrielle Baltazar <jgsbaltazar@yahoo.com>
2017-03-27 08:54:41 -0700

    list

---
+++ Week4/todoproject/src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './App.css';
+
+export default React.createClass({
+  getInitialState: function() {
+    return {
+      list: [],
+      text: ''
+    }
+  },
+
+  handleChange: function(e) {
+      this.setState({
+        text: e.target.value
+      })
+  },
+  handleSubmit: function(e) {
+    console.log('hello')
+    e.preventDefault()
+    this.setState({
+      list: [this.state.text,...this.state.list],
+      text: ''
+    })
+  },
+  // handleCheck: function(list) {                 // for checkbox
+  //   this.state.list.filter(function(list){
+  //     completed: this.state.completed
+  //   }),
+  //   this.setState({
+  //     list: this.state.list
+  //   })
+  // },
+  render: function() {
+  
+    return (
+      <div id="container">
+        <h1>todo</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} type="text" placeholder="What needs to be done?" value={this.state.text}/>
+          </form>
+
+          <div id="taskBody">
+            <ul>
+                {this.state.list.map(function(val,i){
+                return <li key={i}>{val}</li>
+              })}
+            </ul>
+          </div> 
+      </div>
+
+    )
+  }
+})
